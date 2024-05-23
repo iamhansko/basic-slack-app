2fee2e1b512798d692f6d8fae82ab4b35af13615
iamhansko <iamhansko@kakao.com>
2024-05-23 15:45:36 +0900

    fix: Simplify app_mention Event Handler

--- listeners/events/index.js
+++ listeners/events/index.js
@@ -4,10 +4,8 @@ module.exports.register = (app) => {
   app.event('app_mention', async ({ event, context, say }) => {
     const { botUserId } = context;
     const textInput = event.text.replaceAll(`<@${botUserId}> `, '');
-    const temp = Hangul.disassemble(textInput).map((item) => item.replace('ㅏ', 'ㅡ').replace('ㅜ', 'ㅡ'));
-    const textOutput = Hangul.assemble(temp);
-    const randomNumber1 = Math.floor(Math.random() * 100);
-    const randomNumber2 = Math.floor(Math.random() * 50);
+    const textTransform = Hangul.disassemble(textInput).map((item) => item.replace('ㅏ', 'ㅡ').replace('ㅜ', 'ㅡ'));
+    const textOutput = Hangul.assemble(textTransform);
 
     await say({
       blocks: [
@@ -15,7 +13,7 @@ module.exports.register = (app) => {
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: `${randomNumber1 >= 30 ? '에베베베베베 ' : ''}${textOutput}${randomNumber2 >= 30 ? '~' : ''}`,
+            text: `${textOutput}${Math.floor(Math.random() * 5) >= 3 ? '~' : ''}`,
           },
         },
       ],
